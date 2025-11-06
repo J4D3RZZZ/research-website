9704d4a7a1e0b12ad9736317cbe756d6dd1e0e2d
J4D3RZZZ <124880962+J4D3RZZZ@users.noreply.github.com>
2025-11-07 01:26:40 +0900

    Delete backend directory

--- backend/routes/roomRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// Temporary route for testing
-router.get("/", (req, res) => {
-  res.send("API route working!");
-});
-
-export default router;
